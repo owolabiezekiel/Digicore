1d23686a83244a3f73453827e9b69e7f3f517630
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2021-06-22 23:20:39 +0100

    Initial commit

---
+++ src/test/java/owolabi/ezekiel/digicore/DigicoreApplicationTests.java
@@ -0,0 +1,13 @@
+package owolabi.ezekiel.digicore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigicoreApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
