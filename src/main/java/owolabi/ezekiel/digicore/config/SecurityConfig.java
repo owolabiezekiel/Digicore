a0d1fea51087fff33b6bdaf386a8c4a0f38c9960
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2021-08-10 14:30:26 +0100

    Swagger works with authentication after ant-matching a host of urls that swagger needed but were being blocked by spring security

--- src/main/java/owolabi/ezekiel/digicore/config/SecurityConfig.java
+++ src/main/java/owolabi/ezekiel/digicore/config/SecurityConfig.java
@@ -27,6 +27,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
+  private static final String[] AUTH_WHITELIST = {
+      // -- Swagger UI v2
+      "/v2/api-docs",
+      "/swagger-resources",
+      "/swagger-resources/**",
+      "/configuration/ui",
+      "/configuration/**",
+      "/configuration/security",
+      "/swagger-ui.html",
+      "/webjars/**",
+      // -- Swagger UI v3 (OpenAPI)
+      "/v3/api-docs/**",
+      "/swagger-ui/**",
+      // other public endpoints of your API may be appended to this array
+      "/auth/*"
+  };
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -35,8 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .exceptionHandling().authenticationEntryPoint(unathorizedHandler)
         .and()
         .authorizeRequests()
-        .antMatchers("*/swagger-ui.html#/*").permitAll()
-        .antMatchers("/auth/*").permitAll()
+        .antMatchers(AUTH_WHITELIST).permitAll()
         .anyRequest().authenticated()
         .and()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
