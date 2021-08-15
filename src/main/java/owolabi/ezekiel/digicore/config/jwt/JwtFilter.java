c772e54850b04fafa4f2ec083b793619fb7befa9
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2021-08-15 23:59:35 +0100

    fixed issues

--- src/main/java/owolabi/ezekiel/digicore/config/jwt/JwtFilter.java
+++ src/main/java/owolabi/ezekiel/digicore/config/jwt/JwtFilter.java
@@ -4,15 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.filter.OncePerRequestFilter;
 import owolabi.ezekiel.digicore.CustomUserDetailsService;
 import owolabi.ezekiel.digicore.config.CustomUserDetails;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
--- src/main/java/owolabi/ezekiel/digicore/config/jwt/JwtProvider.java
+++ src/main/java/owolabi/ezekiel/digicore/config/jwt/JwtProvider.java
@@ -6,7 +6,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
